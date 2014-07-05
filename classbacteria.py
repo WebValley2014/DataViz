c5b8722c350fdd25bdccd5a9404c83f187647ecc
ste96 <stefano.valentini1996@gmail.com>
2014-07-05 10:16:56 +0200

    7 graphs in the section metricsplot, MCC, SPEC...

--- classbacteria.py
+++ classbacteria.py
@@ -12,7 +12,7 @@ def percentage_row(matrix, style='row'):
 	if style != 'row':
 		mc = mc.T
 	#computing sum row by row
-	return mc / mc.sum(axis=1).reshape(2, 1)
+	return mc / mc.sum(axis=1).reshape(-1, 1)
 	
 class BacteriaGraph(object):
 	"""
@@ -53,16 +53,17 @@ class BacteriaGraph(object):
 	def percentagehistogramm(self):
 		#CREATION OF THE PERCENTAGE HISTOGRAMM OF THE DATA WITH RANDOM COLORS
 		#clculate the percentage
-		
+		percentageData = percentage_row(self.data1)
 		width = 0.8
 		bottom = [0] * self.samplesCount
 		colors = [0] * self.bacteriaCount
+		rangeNum = np.arange(self.bacteriaCount + 1)
 		n = 0
 		for i in percentageData:
 			if n != 0:
 				#sum the botton at the last bottom
 				cont = 0
-				while cont < self.samplesCount:
+				while cont < self.samplesCount + 1:
 	  	      			bottom[cont] = bottom[cont] + hist[cont]
 					cont += 1
 			hist = i
@@ -77,11 +78,33 @@ class BacteriaGraph(object):
 		plt.savefig('histplot', format = 'png')
 	def metricsplot(self, style=1):
 		self.style = style
+		#definition of the columns of the file
 		colRep = 0
 		colMMC = 1
 		colMMCmin = 2
 		colMMCmax = 3
-		numData = 3694 #numero di analisi
+		colSENS = 4
+		colSENSmin = 5
+		colSENSmax = 6
+		colSPEC = 7
+		colSPECmin = 8
+		colSPECmax = 9
+		colPPV = 10
+		colPPVmin = 11
+		colPPVmax = 12
+		colNPV = 13
+		colNPVmin = 14
+		colNPVmax = 15
+		colAUC = 16
+		colAUCmin = 17
+		colAUCmax = 18
+		colACC = 19
+		colACCmin = 20
+		colACCmax = 21
+		colDOR = 22
+		colDORmin = 23
+		colDORmax = 24
+		numData = 3694 #number of analysis
 		rowData = self.metrics
 		if style == 1:
 			rowData = self.metrics[:10]
@@ -92,21 +115,79 @@ class BacteriaGraph(object):
 		elif style == 3:
 			rowData = self.metrics[:1000]
 			numData = 1000
+		#definition of the array
+		arrRep = np.zeros(numData + 1)
 		arrMCC = np.zeros(numData + 1)
 		arrMCCerr = np.zeros(numData + 1)
-		arrRep = np.zeros(numData + 1)
+		arrSENS = np.zeros(numData + 1)
+		arrSENSerr = np.zeros(numData + 1)
+		arrSPEC = np.zeros(numData + 1)
+		arrSPECerr = np.zeros(numData + 1)
+		arrPPV = np.zeros(numData + 1)
+		arrPPVerr = np.zeros(numData + 1)
+		arrNPV = np.zeros(numData + 1)
+		arrNPVerr = np.zeros(numData + 1)
+		arrAUC = np.zeros(numData + 1)
+		arrAUCerr = np.zeros(numData + 1)
+		arrACC = np.zeros(numData + 1)
+		arrACCerr = np.zeros(numData + 1)
+		arrDOR = np.zeros(numData + 1)
+		arrDORerr = np.zeros(numData + 1
 		n = 0
 		for i in rowData:
 			arr = i
 			arrRep[n] = arr[colRep]
 			arrMCC[n] = arr[colMMC]
 			arrMCCerr[n] = (arr[colMMCmax] - arr[colMMCmin])/2
+			arrSENS[n] = arr[colSENS]
+			arrSENSerr[n] = (arr[colSENSmax] - arr[colSENSmin])/2
+			arrSPEC[n] = arr[colSPEC]
+			arrSPECerr[n] = (arr[colSPECmax] - arr[colSPECmin])/2
+			arrPPV[n] = arr[colPPV]
+			arrPPVerr[n] = (arr[colPPVmax] - arr[colPPVmin])/2
+			arrNPV[n] = arr[colNPV]
+			arrNPVerr[n] = (arr[colNPVmax] - arr[colNPVmin])/2
+			arrAUC[n] = arr[colAUC]
+			arrAUCerr[n] = (arr[colAUCmax] - arr[colAUCmin])/2
+			arrACC[n] = arr[colACC]
+			arrACCerr[n] = (arr[colACCmax] - arr[colACCmin])/2
+			arrDOR[n] = arr[colDOR]
+			arrDORerr[n] = (arr[colDORmax] - arr[colDORmin])/2
 			n += 1
 		plt.errorbar(arrRep, arrMCC, xerr=0, yerr=arrMCCerr)
 		plt.xlim((0.5, numData + 1))
 		plt.subplot().set_xscale("log")
-		plt.savefig('graphs/metricsplot' + str(style), format = 'png')
+		plt.savefig('graphs/MCC_metricsplot' + str(style), format = 'png')
+		plt.errorbar(arrRep, arrSENS, xerr=0, yerr=arrSENSerr)
+		plt.xlim((0.5, numData + 1))
+		plt.subplot().set_xscale("log")
+		plt.savefig('graphs/SENS_metricsplot' + str(style), format = 'png')
+		plt.errorbar(arrRep, arrSPEC, xerr=0, yerr=arrSPECerr)
+		plt.xlim((0.5, numData + 1))
+		plt.subplot().set_xscale("log")
+		plt.savefig('graphs/SPEC_metricsplot' + str(style), format = 'png')
+		plt.errorbar(arrRep, arrPPV, xerr=0, yerr=arrPPVerr)
+		plt.xlim((0.5, numData + 1))
+		plt.subplot().set_xscale("log")
+		plt.savefig('graphs/PPV_metricsplot' + str(style), format = 'png')
+		plt.errorbar(arrRep, arrNPV, xerr=0, yerr=arrNPVerr)
+		plt.xlim((0.5, numData + 1))
+		plt.subplot().set_xscale("log")
+		plt.savefig('graphs/NPV_metricsplot' + str(style), format = 'png')
+		plt.errorbar(arrRep, arrAUC, xerr=0, yerr=arrAUCerr)
+		plt.xlim((0.5, numData + 1))
+		plt.subplot().set_xscale("log")
+		plt.savefig('graphs/AUC_metricsplot' + str(style), format = 'png')
+		plt.errorbar(arrRep, arrACC, xerr=0, yerr=arrACCerr)
+		plt.xlim((0.5, numData + 1))
+		plt.subplot().set_xscale("log")
+		plt.savefig('graphs/ACC_metricsplot' + str(style), format = 'png')
+		plt.errorbar(arrRep, arrDOR, xerr=0, yerr=arrDORerr)
+		plt.xlim((0.5, numData + 1))
+		plt.subplot().set_xscale("log")
+		plt.savefig('graphs/DOR_metricsplot' + str(style), format = 'png')
 		plt.clf()
+'''
 	def classbacteriahistogramm():
 		#calculate the number of classes
 		
@@ -127,13 +208,14 @@ class BacteriaGraph(object):
 		avBacteria = np.zeros((self.bacteriaCount, numCases))
 		for i in self.classes:
 			pass
+'''
 
 if __name__ == "__main__":
 
 
 	a=BacteriaGraph('fakedata/percentagebacteria.txt', 'fakedata/P2', 'fakedata/metrics.txt')
 	a.metricsplot(3)
-	#a.metricsplot(2)
-	#a.metricsplot(4)
-	#a.metricsplot(1)
-	a.percentagehistogramm()
+	a.metricsplot(2)
+	a.metricsplot(4)
+	a.metricsplot(1)
+	#a.percentagehistogramm()
